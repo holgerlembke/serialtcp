87a08ddbfead96d0f24ca587dcab9edf1ea05745
holgerlembke <lembke@gmail.com>
2016-11-01 13:13:59 +0100

    Add files via upload

--- serialtcp/serialtcp.cpp
+++ serialtcp/serialtcp.cpp
@@ -2,30 +2,58 @@
 #include <inttypes.h>
 #include "serialTCP.h"
 
+serialTCP SerialTCP;
+
 //*************************************************************************************
 serialTCP::serialTCP() {}
+
+serialTCP::~serialTCP() {
+  closetcp();
+}
 
 //*************************************************************************************
 size_t serialTCP::write(uint8_t data)
 {
+  if (! _client) {
+    opentcp();	
+  }
+
+  if (_client) {
+    _client.write(data);
+  }	
+	
   Serial.write(data);
 
   return 1;
 }
 
+//*************************************************************************************
 void serialTCP::opentcp()
 {
-	
-}
-
-void serialTCP::closetcp()
-{
-	
+  if (_ip!="") {
+    if (!_client.connect(_ip.c_str(),_port)) {
+      Serial.println("Debug cant open ");
+	  if (! _reconnect) {
+		  _ip="";
+	  }
+      return;
+    }
+  }
 }
 
 //*************************************************************************************
-void serialTCP::begin(long baud, String ip, uint16_t port)
+void serialTCP::closetcp()
+{
+  if (_client) {
+    _client.stop();
+  }
+}
+
+//*************************************************************************************
+void serialTCP::begin(long baud, String ip, uint16_t port, bool reconnect)
 {
 	_port=port;
 	_ip=ip;
+	_reconnect=reconnect;
+	closetcp();
 }
--- serialtcp/serialtcp.h
+++ serialtcp/serialtcp.h
@@ -1,24 +1,31 @@
 // inline guard. Did I mention that c/c++ is broken by design?
-#ifndef LPT_h
-#define LPT_h
+#ifndef serialTCP_h
+#define serialTCP_h
 
 #include "Arduino.h"
 #include <inttypes.h>
 #include "Print.h"
+#include <ESP8266WiFi.h>
+
 
 class serialTCP : public Print
 {
   private:
     String _ip;
 	uint16_t _port;
+    WiFiClient _client;
+	bool _reconnect;	
 	
 	void opentcp();
 	void closetcp();
   public:
     serialTCP();
+	virtual ~serialTCP();
     size_t write(uint8_t data);
 
-    void begin(long baud, String ip, uint16_t port);
+    void begin(long baud, String ip, uint16_t port, bool reconnect = true);
 };
+
+extern serialTCP SerialTCP;
 
 #endif
