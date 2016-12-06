34573bbe2bc566e827d81b4a714fbb657d5e4758
holgerlembke <lembke@gmail.com>
2016-12-06 19:19:00 +0100

    Add files via upload
    Limiting the maximum paket size.

--- serialtcp/serialtcp.cpp
+++ serialtcp/serialtcp.cpp
@@ -10,8 +10,8 @@ serialTCP SerialTCP;
 //=====================================================================================
 //=====================================================================================
 serialUDP::serialUDP() {
-  _ip="";
-  _port=0;
+	_ip="";
+	_port=0;
 }
 
 serialUDP::~serialUDP() {
@@ -20,49 +20,59 @@ serialUDP::~serialUDP() {
 //=====================================================================================
 size_t serialUDP::write(uint8_t data)
 {
-  _client.beginPacket(_ip.c_str(),_port); 
-  _client.write(&data,sizeof(data));
-  _client.endPacket();
-  delay(1);
-    
-  Serial.write(data);
+	_client.beginPacket(_ip.c_str(),_port); 
+	_client.write(&data,sizeof(data));
+	_client.endPacket();
+	delay(1);
+	
+	Serial.write(data);
 
-  return 1;
+	return 1;
 }
+
+#define udpsendlimit 512  // more or less random, I found no data for the esp8266.
 
 //=====================================================================================
 size_t serialUDP::write(const char *str)
 {
-  if (str == NULL) return 0; 
+	if (str == NULL) return 0; 
 
-  _client.beginPacket(_ip.c_str(),_port); 
-  _client.write((const uint8_t *)str, strlen(str));
-  _client.endPacket();
-  delay(1);
-    
-   return Serial.write((const uint8_t *)str, strlen(str)); 
+	_client.beginPacket(_ip.c_str(),_port); 
+	int i = strlen(str);
+	if (i>udpsendlimit) {
+		i=udpsendlimit;
+	}
+	_client.write((const uint8_t *)str, i);
+	_client.endPacket();
+	delay(1);
+	
+	return Serial.write((const uint8_t *)str, strlen(str)); 
 }
 
 //=====================================================================================
 size_t serialUDP::write(const uint8_t *buffer, size_t size)
 {
-  _client.beginPacket(_ip.c_str(),_port); 
-  _client.write(buffer,size);
-  _client.endPacket();
-  delay(1);
+	_client.beginPacket(_ip.c_str(),_port); 
+	int i = size;
+	if (i>udpsendlimit) {
+		i=udpsendlimit;
+	}
+	_client.write(buffer,i);
+	_client.endPacket();
+	delay(1);
 
-  return Serial.write((const uint8_t *)buffer, size);
+	return Serial.write((const uint8_t *)buffer, size);
 }
 
 //=====================================================================================
 void serialUDP::begin(long baud, String ip, uint16_t port, bool reconnect)
 {
-  _port=port;
-  _ip=ip;
+	_port=port;
+	_ip=ip;
 	
-  if (baud>0) {
-    Serial.begin(baud);
-  }
+	if (baud>0) {
+		Serial.begin(baud);
+	}
 }
 
 
@@ -71,92 +81,92 @@ void serialUDP::begin(long baud, String ip, uint16_t port, bool reconnect)
 //*************************************************************************************
 serialTCP::serialTCP() {
 	_reconnect=true;
-    _ip="";
-    _port=0;
+	_ip="";
+	_port=0;
 }
 
 serialTCP::~serialTCP() {
-  closetcp();
+	closetcp();
 }
 
 //*************************************************************************************
 size_t serialTCP::write(uint8_t data)
 {
-  if (! _client) {
-    opentcp();  
-  }
+	if (! _client) {
+		opentcp();  
+	}
 
-  if (_client) {
-    _client.write(data);
-  } 
-    
-  Serial.write(data);
+	if (_client) {
+		_client.write(data);
+	} 
+	
+	Serial.write(data);
 
-  return 1;
+	return 1;
 }
 
 //*************************************************************************************
 size_t serialTCP::write(const char *str)
 {
-  if (str == NULL) return 0; 
+	if (str == NULL) return 0; 
 
-  if (! _client) {
-    opentcp();  
-  }
+	if (! _client) {
+		opentcp();  
+	}
 
-  if (_client) {
-    _client.write((const uint8_t *)str, strlen(str)); 
-  } 
-    
-   return Serial.write((const uint8_t *)str, strlen(str)); 
+	if (_client) {
+		_client.write((const uint8_t *)str, strlen(str)); 
+	} 
+	
+	return Serial.write((const uint8_t *)str, strlen(str)); 
 }
 
 //*************************************************************************************
 size_t serialTCP::write(const uint8_t *buffer, size_t size)
 {
-  if (! _client) {
-    opentcp();  
-  }
+	if (! _client) {
+		opentcp();  
+	}
 
-  if (_client) {
-    _client.write((const uint8_t *)buffer, size);
-  } 
-    
-  return Serial.write((const uint8_t *)buffer, size);
+	if (_client) {
+		_client.write((const uint8_t *)buffer, size);
+	} 
+	
+	return Serial.write((const uint8_t *)buffer, size);
 }
 
 
 //*************************************************************************************
 void serialTCP::opentcp()
 {
-  if (_ip!="") {
-    if (!_client.connect(_ip.c_str(),_port)) {
-      Serial.println("Debug cant open ");
-      if (! _reconnect) {
-          _ip="";
-      }
-      return;
-    }
-  }
+	if (_ip!="") {
+		if (!_client.connect(_ip.c_str(),_port)) {
+			Serial.println("Debug cant open ");
+			if (! _reconnect) {
+				_ip="";
+			}
+			return;
+		}
+	}
 }
 
 //*************************************************************************************
 void serialTCP::closetcp()
 {
-  if (_client) {
-    _client.stop();
-  }
+	if (_client) {
+		_client.stop();
+	}
 }
 
 //*************************************************************************************
 void serialTCP::begin(long baud, String ip, uint16_t port, bool reconnect)
 {
-    _port=port;
-    _ip=ip;
-    _reconnect=reconnect;
-    closetcp();
+	_port=port;
+	_ip=ip;
+	_reconnect=reconnect;
+	closetcp();
 	
 	if (baud>0) {
-	  Serial.begin(baud);
+		Serial.begin(baud);
 	}
 }
