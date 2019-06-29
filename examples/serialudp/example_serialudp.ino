214bfc23596c876414acc4119708c8956e788bfe
Holger Lembke <lembke@gmail.com>
2019-06-29 13:55:56 +0200

    Add files via upload

--- examples/serialudp/example_serialudp.ino
+++ examples/serialudp/example_serialudp.ino
@@ -1,1 +1,39 @@
-f
+#ifdef ARDUINO_ARCH_ESP32
+#include <WiFi.h>
+#else
+#ifdef ARDUINO_ARCH_ESP8266
+#include <ESP8266WiFi.h>
+#else
+#error I don't know this Wifi-platform.  
+#endif
+#endif
+
+#include <serialtcp.h>
+
+// This will compile for "Wifi" enabled plattforms online.
+// For me these are ESP8266 and ESP32 currently. Sorry. Send me hardware if you want to change it.
+
+void setup() {
+  // the usual Serial stuff....
+  Serial.begin(9600);
+  while (! Serial) {
+    delay(1);
+  }
+  Serial.print(F("Hello World!"));
+
+  // login into WiFi
+  WiFi.begin("ssid","pwd");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1);
+  }
+
+  // after connecting into the WiFi, open it again
+  SerialUDP.begin(9600,"1.2.3.4",2345);
+  SerialUDP.print(F("Hello again"));
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
